ccffc155226ecc104a5003fe978af6cb9b8c9e3e
MadhuSaini22 <madhusaini46810@gmail.com>
2023-07-31 16:27:50 +0530

    remove: unwanted components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import * as React from "react";
 import "./App.css";
-import DatePicker from "./components/generic/DatePicker";
 import PaginationComponent from "./components/generic/Pagination";
 
 function App() {
   return (
     <div style={{ width: "100vw" }}>
       <PaginationComponent />
-      <DatePicker />
     </div>
   );
 }
